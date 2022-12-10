d8f74e765b0f602ee7fc1404a2d5a52543ea0fd0
David Coletta <david@colettas.org>
2022-12-10 12:35:11 -0500

    Add comments and move color out into variable.

--- MenuHighlighter/MenuHighlighter.js
+++ MenuHighlighter/MenuHighlighter.js
@@ -1,3 +1,12 @@
+/*
+    Name: MenuHighlighter.js
+    What it does: adds a background highlight to the selected link in your app header
+    How to use: add <script src="https://scissors.appspan.net/MenuHighlighter/MenuHighlighter.js"> 
+                to your site's Code Inside Footer
+    How to customize: change the value of the backgroundColor variable
+*/
+const backgroundColor = "#C4B8DA";  // change this value to customize the background color
+
 const nav = $("nav");
 const currentSlug = document.location.pathname;
 const anchors = nav.find("a");
@@ -12,7 +21,7 @@ anchors.each(function() {
   // Otherwise, if the current slug starts with this link,
   // apply some CSS to it.
   if (currentSlug.startsWith(anchorHref)) {
-    anchor.css("background-color","#C4B8DA");
+    anchor.css("background-color", backgroundColor);
     return false;
   }
   return true;
